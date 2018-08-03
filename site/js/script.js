72a00553100e66e3e5f292ef5626325f929a8f03
frank <frank1314168@gmail.com>
2018-08-03 23:01:08 +0800

    2018/08/03 course46

--- site/js/script.js
+++ site/js/script.js
@@ -1,7 +1,27 @@
-function orderChickenWith(sideDish = "whatever!") {
-	// sideDish = sideDish || "whatever!";
-	console.log("Chicken with " + sideDish);
+function multiply(x, y) {
+	return x * y;
+}
+multiply.version = "v.1.0.0";
+console.log(multiply.version);
+
+function makeMultiplier(multiplier){
+	var myFunc = function (x) {
+		return multiplier * x;
+	};
+
+	return myFunc;
 }
 
-orderChickenWith("noodles");
-orderChickenWith();
+var multiplyBy3 = makeMultiplier(3);
+console.log(multiplyBy3(10));
+var doubleAll = makeMultiplier(2);
+console.log(doubleAll(100));
+
+function doOperationOn(x, operation) {
+	return operation(x);
+}
+
+var result = doOperationOn(5,multiplyBy3);
+console.log(result);
+result = doOperationOn(100, doubleAll);
+console.log(result);
