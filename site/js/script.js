0f73b72dfa02d0483f7aba300fe984f3cac39cd5
frank <frank1314168@gmail.com>
2018-08-04 16:15:47 +0800

    2018/08/04 sat course49

--- site/js/script.js
+++ site/js/script.js
@@ -1,27 +1,18 @@
-function multiply(x, y) {
-	return x * y;
-}
-multiply.version = "v.1.0.0";
-console.log(multiply.version);
+var literalCircle = { // new object
+	radius: 10,
 
-function makeMultiplier(multiplier){
-	var myFunc = function (x) {
-		return multiplier * x;
-	};
+	getArea: function () {
+		var self = this;
+		console.log(this);
 
-	return myFunc;
-}
+		var increaseRadius = function () {
+			self.radius = 20;
+		};
+		increaseRadius();
+		console.log(this.radius);
 
-var multiplyBy3 = makeMultiplier(3);
-console.log(multiplyBy3(10));
-var doubleAll = makeMultiplier(2);
-console.log(doubleAll(100));
+		return Math.PI * Math.pow(this.radius, 2);
+	}
+};
 
-function doOperationOn(x, operation) {
-	return operation(x);
-}
-
-var result = doOperationOn(5,multiplyBy3);
-console.log(result);
-result = doOperationOn(100, doubleAll);
-console.log(result);
+console.log(literalCircle.getArea());
