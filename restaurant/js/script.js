f764bca5307db06a6a04af3b7ab52c3795c8624d
frank <frank1314168@gmail.com>
2018-08-09 01:07:37 +0800

    2018/08/09 thu course62 http change to https

--- restaurant/js/script.js
+++ restaurant/js/script.js
@@ -17,11 +17,11 @@ $(function () {//same as document.addeventlistener("domcontentloaded")
 	var dc = {};
 	var homeHtml = "snippets/home-snippet.html";
 	var allCategoriesUrl = 
-		"http://davids-restaurant.herokuapp.com/categories.json";
+		"https://davids-restaurant.herokuapp.com/categories.json";
 	var categoriesTitleHtml = "snippets/categories-title-snippet.html";
 	var categoryHtml = "snippets/category-snippet.html";
 	var menuItemsUrl = 
-		"http://davids-restaurant.herokuapp.com/menu_items.json?category=";
+		"https://davids-restaurant.herokuapp.com/menu_items.json?category=";
 	var menuItemsTitleHtml = "snippets/menu-items-title.html";
 	var menuItemHtml = "snippets/menu-item.html";
 
